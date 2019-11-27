116c0f58d719afb98ac07f3a08084b6cad358c23
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2019-11-27 12:54:01 +0100

    react-stack navigation

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,17 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+
+
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import MealDetailScreen from "../screens/MealDetailScreen";
+
+const MealsNavigator = createStackNavigator({
+  Categories: CategoriesScreen, //forms our home screen
+  CategoryMeals: {
+    screen: CategoryMealsScreen
+  },
+  MealDetail: MealDetailScreen
+});
+
+export default createAppContainer(MealsNavigator);
